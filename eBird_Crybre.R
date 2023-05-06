c3da9080a9093ca00cf46e6a952b96e906607e61
Orlando Acevedo-Charry <acevedocharry@gmail.com>
2023-05-06 19:04:56 -0400

    Update eBird_Crybre.R

--- eBird_Crybre.R
+++ eBird_Crybre.R
@@ -20,13 +20,13 @@ f_ebd <- "ebd_Crybre_OAC.txt"#Data to be saved
 #Filtering the presence data
 ebd_filt<-auk_ebd("ebd_rustin1_relMar-2023/ebd_rustin1_relMar-2023.txt") %>%
   #spatially subsample data that are <5 km long
-#  auk_distance(distance = c(0,5)) %>% 
+#  auk_distance(distance = c(0,5)) %>%            #Remove the numeral at the beginning if you want to filter lists >5km
   #only keep checklists that are less than 5 hours
-#  auk_duration(duration = c(0,300))%>% 
+#  auk_duration(duration = c(0,300))%>%           #Remove the numeral at the beginning if you want to filter lists >5hrs
   #Exclude Historical ISS
-#  auk_protocol(c("Traveling", "Stationary")) %>% 
+#  auk_protocol(c("Traveling", "Stationary")) %>% #Remove the numeral at the beginning if you want to filter lists with detailed effort protocol
   #To provide data with no detections
-#  auk_complete() %>%
+#  auk_complete() %>%.                            #Remove the numeral at the beginning if you want to filter complete list (detection/no detection)
   auk_filter(f_ebd,overwrite=T)
 
 #and with read_ebd I apply another filter to do not repeate records from groups
