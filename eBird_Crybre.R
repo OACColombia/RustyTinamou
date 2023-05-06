28c30b07dc135c21fcb2c8639a4e85608511dc16
Orlando Acevedo-Charry <acevedocharry@gmail.com>
2023-05-06 18:55:33 -0400

    Code to access eBird records (already downloaded)
    With this code, I use the data dowloaded from eBird (ebd) and filter to create two files: one without effort (50 records, same reported in the page) and other using best practices (complete lists within 5 km and less than 5 hrs, with less than 10 observers).

---
+++ eBird_Crybre.R
@@ -0,0 +1,49 @@
+#####   Confirmation distributional hypothesis of birds of Colombia   ####
+
+####~Packages~####
+library(auk) #eBird data filters
+library(tidyverse) #manipulate data
+library(ggmap)
+
+####################
+####~ eBird data ~####
+####################
+
+#Data downloaded from eBird, up to December 2022
+
+#reference to an ebd in preparatio for filtering using awk
+auk_ebd("ebd_rustin1_relMar-2023/ebd_rustin1_relMar-2023.txt")
+
+##I create a path to a new polygon with to save my filter ()
+f_ebd <- "ebd_Crybre_OAC.txt"#Data to be saved
+
+#Filtering the presence data
+ebd_filt<-auk_ebd("ebd_rustin1_relMar-2023/ebd_rustin1_relMar-2023.txt") %>%
+  #spatially subsample data that are <5 km long
+#  auk_distance(distance = c(0,5)) %>% 
+  #only keep checklists that are less than 5 hours
+#  auk_duration(duration = c(0,300))%>% 
+  #Exclude Historical ISS
+#  auk_protocol(c("Traveling", "Stationary")) %>% 
+  #To provide data with no detections
+#  auk_complete() %>%
+  auk_filter(f_ebd,overwrite=T)
+
+#and with read_ebd I apply another filter to do not repeate records from groups
+ebd_only <- read_ebd(f_ebd) 
+
+write.csv(ebd_only, "Crypbrev_NoEffort.csv") #also Crypbrev_effort5x5complete.csv
+
+#Map records
+am <- c(left = -80, bottom = -15, right = -40, top = 15)
+AmericaMap <- get_stamenmap(am, zoom = 5, maptype = "toner-lite") %>% 
+  ggmap()
+
+AmericaMap +
+  geom_point(data = ebd_only,
+             aes(x = longitude, y = latitude), 
+             shape = 21, 
+             alpha = 0.25)+
+  theme_classic() + 
+  labs(x = "Longitude", y = "Latitude", fill = "") +
+  theme(legend.position = "bottom")
